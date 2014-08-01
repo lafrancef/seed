5557aac549920e2ddc2b5ce29fb1df355d4a320f
Fred Lafrance <lafrancef@gmail.com>
2014-08-01 15:26:41 -0400

    Improved the way Node/Trace constructors work

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,3 +1,13 @@
 class Node < ActiveRecord::Base
   belongs_to :tree
+  
+  def to_s
+    return "id: " + self.relative_id.to_s +
+			", x: " + self.x.to_s +
+			", y: " + self.y.to_s +
+			", pos: \"" + self.part_of_speech.to_s +
+			"\", contents: \"" + self.contents.to_s +
+			"\", theta: " + self.theta.to_s +
+			", caseMarker: " + self.case_marker.to_s
+  end
 end
--- app/models/trace.rb
+++ app/models/trace.rb
@@ -1,3 +1,6 @@
 class Trace < Node
-  
+  def to_s
+    return super.to_s + 
+	        ", trace_idx: " + trace_idx.to_s
+  end
 end
